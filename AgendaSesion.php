8f2100bc240a9cd2ab0a71cb99bb42108d8544ad
XArtola <xartolaza18dw@ikzubirimanteo.com>
2019-10-07 18:39:37 +0200

    He controlado mayusculas y acentos

--- AgendaSesion.php
+++ AgendaSesion.php
@@ -2,6 +2,54 @@
 
 session_start();
 
+$listaContactos = "hola";
+$error =" ";
+
+function mostrarNombre(){
+    if(empty($_GET['nombre']))
+        return "";
+    else
+        return $_GET['nombre'];
+}
+
+function mostrarMail(){
+    if(empty($_GET['email']))
+        return "";
+    else
+        return $_GET['email'];
+}
+
+if (isset($_GET['submit'])) {
+    insertarDatos($_GET['nombre'], $_GET['email']);
+}
+
+function insertarDatos($nombre, $email)
+{
+
+    if (empty($nombre))
+
+        $GLOBALS['error'] .= "Error nombre vacio";
+    
+    else if(!empty($nombre) and !empty($email)) {
+
+        $nombre = strtolower($nombre);
+
+        $nombre = str_replace(
+            array('á', 'é', 'í', 'ó', 'ú'),
+            array('a', 'e', 'i', 'o', 'u'),
+            $nombre
+        );
+
+        $_SESSION['Lista'][$nombre] = "$email";
+
+        foreach ($_SESSION['Lista'] as $clave => $valor)
+
+            $GLOBALS['listaContactos'] .="$clave $valor<br>";   
+    }   
+}
+
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,8 +71,8 @@ session_start();
                 <legend>
                     Información de contacto
                 </legend>
-                <label>Nombre: </label><input id="nombre" name="nombre" tyrp="text" placeholder="Introduce tu nombre" value="<?php echo cogerNombre(); ?>"><br>
-                <label>Email: </label><input id="email" name="email" tyrp="email" placeholder="Introduce tu dirección de correo" value="<?php echo cogerMail(); ?>">
+                <label>Nombre: </label><input id="nombre" name="nombre" tyrp="text" placeholder="Introduce tu nombre" value="<?php echo mostrarNombre(); ?>"><br>
+                <label>Email: </label><input id="email" name="email" tyrp="email" placeholder="Introduce tu dirección de correo" value="<?php echo mostrarMail(); ?>">
                 <br>
                 <input type="submit" name="submit" value="Insertar datos">
             </fieldset>
@@ -35,57 +83,9 @@ session_start();
     <aside>
         <h2>Lista de contactos</h2>
         <p id="listado">
-
-            <?php
-
-           $lista = "";
-
-            function cogerNombre(){
-                if(empty($_GET['nombre']))
-                    return "";
-                else
-                    return $_GET['nombre'];
-            }
-
-            function cogerMail(){
-                if(empty($_GET['email']))
-                    return "";
-                else
-                    return $_GET['email'];
-            }
-
-            if (isset($_GET['submit'])) {
-                insertarDatos($_GET['nombre'], $_GET['email']);
-            }
-
-            function insertarDatos($nombre, $email)
-            {
-
-
-                $prueba = "1111";
-                if (empty($nombre) or empty($email))
-
-                    echo "error";
-
-              /*  if(strpos($email, '@'))
-                echo "hay arroba"; 
-                */
-                
-                else if(!empty($nombre) and !empty($email)) {
-
-                    $_SESSION['Lista'][$nombre] = "$email";
-
-                    foreach ($_SESSION['Lista'] as $clave => $valor)
-
-                        $lista += "$clave $valor<br>";
-                }
-            }
-
-            ?>
+            <?php echo $GLOBALS['listaContactos']; ?>
         </p>
     </aside>
-    
-
 
 </body>
 
